b33d16ad74f8c2f232eb12bbe75c65f47f721164
khanhs10102000 <khanhs10102000@gmail.com>
2023-02-04 21:15:31 +0700

    fix db cloud

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -7,11 +7,11 @@ const mysql = require('mysql2/promise');
 //   database: 'dat-lich-kham',
 // });
 var pool = mysql.createPool({
-  host: 'mysql-108456-0.cloudclusters.net', // Replace with your host name
+  host: 'mysql-109407-0.cloudclusters.net', // Replace with your host name
   user: 'admin', // Replace with your database username
-  password: 'puiVL2HZ', // Replace with your database password
+  password: '98zHm5J4', // Replace with your database password
   database: 'dat-lich-kham', // // Replace with your database Name
-  port: 10257,
+  port: 10021,
 });
 pool.getConnection((err) => {
   if (err) console.log(JSON.stringify(err));
